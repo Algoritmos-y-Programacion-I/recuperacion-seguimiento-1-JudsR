c196ed17267ed55dcbfea8b1545b9a1dcbc270cb
juan.diego.rp83@gmail.com <juan.diego.rp83@gmail.com>
2025-02-27 18:41:43 -0500

    Ejercicio 1 hecho

--- src/ui/RecuperacionSeguimiento1.java
+++ src/ui/RecuperacionSeguimiento1.java
@@ -1,7 +1,6 @@
 package ui;
 
 import java.util.Scanner;
-import java.lang.Math;
 
 public class RecuperacionSeguimiento1 {
 
@@ -16,6 +15,18 @@ public class RecuperacionSeguimiento1 {
 	
 	public void run() // Aqui en el metodo run hacemos lo de nuestro metodo main. Solo entrada de datos, mensajes al usuario y mostrar salidas
 	{   
+		int n1, n2, n3, paramIguales;
+		
+		System.out.println("Para el ejercicio #1 ingrese tres valores enteros: ");
+		System.out.println("#1: ");
+		n1 = escaner.nextInt();
+		System.out.println("#2: ");
+		n2 = escaner.nextInt();
+		System.out.println("#3: ");
+		n3 = escaner.nextInt();
+		paramIguales = parametrosIguales (n1, n2, n3);
+		System.out.println("Se ingresan "+n1+", "+n2+", "+n3+"; el resultado es "+paramIguales);
+
         // Metodo pow de Math
         double a = 2;
         double b = 3;
@@ -40,8 +51,22 @@ public class RecuperacionSeguimiento1 {
 		mainApp.run();
 	}
 
+	public int parametrosIguales (int n1, int n2, int n3) {
+		int i;
+		
+		i = 0;
+		if (n1 == n2 && n1 == n3) {
+			i+= 3;
+		} else if (n1 == n2 || n1 == n3 || n2 == n3) {
+			i+= 2;
+		}
+
+
+		return i;
+	}
+
     /**
-	 * Descripción: El método ... permite ...
+	 * Descripción: El método parametrosIguales permite evaluar si los numeros ingresados son el mismo
 	 * @param 
 	 * @return 
 	 */
