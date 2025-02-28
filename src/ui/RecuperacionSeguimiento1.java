27de8c44daaa9bdb0b1b1f4216e41f434b0cc363
juan.diego.rp83@gmail.com <juan.diego.rp83@gmail.com>
2025-02-27 21:37:05 -0500

    Va al 4 sin el 3

--- src/ui/RecuperacionSeguimiento1.java
+++ src/ui/RecuperacionSeguimiento1.java
@@ -27,21 +27,42 @@ public class RecuperacionSeguimiento1 {
 		System.out.println("#3: ");
 		n3 = escaner.nextInt();
 		paramIguales = parametrosIguales (n1, n2, n3);
-		System.out.println("Se ingresan "+n1+", "+n2+", "+n3+"; el resultado es "+paramIguales);
+		System.out.println("Se ingresan "+n1+", "+n2+", "+n3+"; el resultado es "+paramIguales+".");
 
 		//
 
 		// Ejercicio 2
-		String nombre;
+		String nombre, mensaje;
 		int n0_1;
 
 		System.out.println("");
 		System.out.println("Para el ejercicio #2 ingrese su nombre por favor: ");
-		nombre = escaner.nextLine();
 		escaner.nextLine();
+		nombre = escaner.nextLine();
 		System.out.println("Ahora ingrese un numero, si es 1 = saludo, 0 = adios: ");
 		n0_1 = validacionE2(nombre);
-		mensajeResultado(n0_1, nombre);
+		mensaje = mensajeResultado(n0_1, nombre);
+		System.out.println(mensaje);
+		//
+
+		//Ejercicio 4
+		String palabra1, palabra2;
+		boolean vOf;
+
+		System.out.println("");
+		System.out.println("Para el ejercicio 4 ingrese dos palabras y validaremos si el caracter de la posicion 2 es el mismo: ");
+		escaner.nextLine();
+		System.out.println("Palabra 1: ");
+		palabra1 = validarPalabra(escaner);
+		System.out.println("Palabra 2: ");
+		palabra2 = validarPalabra(escaner);
+		vOf = validarCaracter2(palabra1, palabra2);
+		System.out.println("Se ingresan "+palabra1+" y "+palabra2+", el resutado es "+vOf+".");
+		//
+
+		// Ejercicio #5
+		System.out.println("Ahora, para el #5 se requiere de 3 numeros.");
+		System.out.println("Con los cuales ecuación cuadrática de la forma: ax^2 + bx + c");
 		
 		
 
@@ -94,28 +115,53 @@ public class RecuperacionSeguimiento1 {
 
 			while (true) {
 				n0_1 = escaner.nextInt();
-				if (n0_1 != 1 || n0_1 != 0) {
-					System.err.println("Se ingresan "+nombre+" y "+n0_1+", el resultado es “Error entradas inválidas");
+
+				if (n0_1 == 1 || n0_1 == 0) {
+					return n0_1;
 				} else {
-					break;
+					System.out.println("Se ingresan "+nombre+" y "+n0_1+", el resultado es: Error, entradas inválidas.");
+					System.out.println("Ingrese el numero nuevamente: ");
 				}
 		}
-		return n0_1;
 	 }
 
 	 public String mensajeResultado(int n0_1, String nombre) {
 		String mensaje;
 		
 		if (n0_1 == 1) {
-			mensaje = ("Se ingresan "+nombre+" y "+n0_1+", el resultado es “Hola "+nombre);
+			mensaje = ("Se ingresan "+nombre+" y "+n0_1+", el resultado es: Hola "+nombre+".");
 			
 		} else{
-			mensaje = ("Se ingresan "+nombre+" y "+n0_1+", el resultado es “Hola "+nombre);
+			mensaje = ("Se ingresan "+nombre+" y "+n0_1+", el resultado es: Hola "+nombre+".");
 			
 		}
 		return mensaje;
 	 }
-	
 
+	 public String validarPalabra(Scanner escaner){
+		String palabra; 
+
+		while (true) {
+			palabra = escaner.nextLine();
+
+			if (palabra.length()>=3 ){
+				return palabra;
+			} else {
+				System.out.println("Palabra no valida, debe ser por lo menos de 3 caracteres.");
+				System.out.println("Ingrese la palabra nuevamente: ");
+			}
+		}
+	 }
+	
+	 public boolean  validarCaracter2(String palabra1, String palabra2){
+		boolean vOf;
+		 
+		if (palabra1.charAt(2) == palabra1.charAt(2)){
+			vOf = true;
+	 	} else{
+			vOf = false;
+		}
+		return vOf;
+	 }
 	
 }
